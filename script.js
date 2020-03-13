6092468c3a0425e6c10e9368b1a39580200eb3d3
viktor <vitek230290@gmail.com>
2020-03-13 13:50:47 +0300

    feat: add send message form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function load() {
    
+    const SUBMIT = document.getElementById('submit');
+    const CLOSE_BTN = document.getElementById('btn-ok');
+
     document.querySelector('.navigation').addEventListener('click', (e) => {
         document.querySelectorAll('.navigation__link').forEach(el => el.classList.remove('active-link'));   
         e.target.classList.add('active-link');
@@ -32,6 +35,14 @@ function load() {
     document.querySelector('.button-container').addEventListener('click', (e) => {
         document.querySelectorAll('.portfolio__btn').forEach(el => el.classList.remove('active'));   
         e.target.classList.add('active');
+
+        const imgArr = document.querySelectorAll('.portfolio__img');
+        imgArr[0].before(imgArr[10]);
+        imgArr[1].before(imgArr[11]);
+        imgArr[2].before(imgArr[3]);
+        imgArr[3].before(imgArr[5]);
+        imgArr[4].before(imgArr[6]);
+       
     })
 
     document.querySelector('.image-container').addEventListener('click', (e) => {
@@ -40,7 +51,35 @@ function load() {
         );
         e.target.classList.add('active-img');
     })
-}
+
+    SUBMIT.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo(pageXOffset, 0);
+        const subject = document.getElementById('subject');
+        const describe = document.getElementById('textarea');
+
+        if (subject.value !== '') {
+            document.getElementById('subject-text').innerText = `Тема: ${subject.value.toString()}`;  
+        } else {
+            document.getElementById('subject-text').innerText = 'Без темы';
+        };
+
+        if (describe.value !== '') {
+            document.getElementById('describe').innerText = `Описание: ${describe.value.toString()}`; 
+        } else {
+            document.getElementById('describe').innerText = 'Без описания';
+        };
+
+        document.getElementById('message-block').classList.remove('hidden');
+    });
+        
+    CLOSE_BTN.addEventListener('click', () => {
+        document.getElementById('subject-text').innerText = '';
+        document.getElementById('describe').innerText = '';
+        document.getElementById('message-block').classList.add('hidden');
+    })
+
+};
 
 window.onload = () => {
     load();
