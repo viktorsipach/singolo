40ea37bf64d32be1887722e2f2b77a3359527da4
viktor <vitek230290@gmail.com>
2020-03-16 02:17:09 +0300

    fix: code readability

--- script.js
+++ script.js
@@ -1,91 +1,150 @@
-function load() {
-   
-    const SUBMIT = document.getElementById('submit');
-    const CLOSE_BTN = document.getElementById('btn-ok');
+window.onload = () => {
+    addClickMenuHandler();
+    addClickTagsHandler();
+    addClickSliderHandler();
+    addClickBlackScreenHandler();
+    addClickImgHandler();
+    addClickSubmitHandler();
+    addClickModalCloseHandler();
+    
+};
 
+const addClickMenuHandler = () => {
     document.querySelector('.navigation').addEventListener('click', (e) => {
-        document.querySelectorAll('.navigation__link').forEach(el => el.classList.remove('active-link'));   
-        e.target.classList.add('active-link');
+        if (e.target.classList.contains('navigation__link')) {
+            let clickedLink = e.target;
+            removeSelectedLink();
+            selectClickedLink(clickedLink);
+        }
     })
+};
 
-    document.querySelector('.home__btn_ver').addEventListener('click', () => {
-        document.querySelector('.black-screen__ver').classList.toggle('hide');
-    })
-    
-    document.querySelector('.home__btn_gor').addEventListener('click', () => {
-        document.querySelector('.black-screen__gor').classList.toggle('hide');
-    })
+const removeSelectedLink = () => {
+    let links = document.querySelectorAll('.navigation__link');
+    links.forEach(link => {
+        link.classList.remove('active_link');
+    });
+};
 
-    document.querySelector('.home__btn').addEventListener('click', () => {
-        document.querySelector('.black-screen').classList.toggle('hide');
-    })
-    
-    document.getElementById('btn-left').addEventListener('click', () => {
-        document.querySelector('.slide-first__container').classList.toggle('hide-slide');
-        document.querySelector('.slide-second__container').classList.toggle('show-slide');
-        document.querySelector('.slide-second__container').classList.toggle('hide-slide');
-    })
+const selectClickedLink = (link) => {
+    link.classList.add('active_link');
+};
 
-    document.getElementById('btn-right').addEventListener('click', () => {
-        document.querySelector('.slide-first__container').classList.toggle('hide-slide');
-        document.querySelector('.slide-second__container').classList.toggle('show-slide');
-        document.querySelector('.slide-second__container').classList.toggle('hide-slide');
+const addClickTagsHandler = () => {
+    document.querySelector('.tags-container').addEventListener('click', (e) => {
+        if (e.target.classList.contains('portfolio__tag')) {
+            let clickedTag = e.target;
+            removeSelectedTag();
+            selectClickedTag(clickedTag);
+            changeImgPosition();
+        }
     })
+};
 
-    document.querySelector('.button-container').addEventListener('click', (e) => {
-        document.querySelector('.button-container').classList.toggle('active');
-        document.querySelectorAll('.portfolio__btn').forEach(el => el.classList.remove('active'));   
-        e.target.classList.add('active');
+const removeSelectedTag = () => {
+    let buttons = document.querySelectorAll('.portfolio__tag');
+    buttons.forEach(btn => {
+        btn.classList.remove('active_tag');
+    });
+};
 
-        const imgArr = document.querySelectorAll('.portfolio__img');
+const selectClickedTag = (btn) => {
+    btn.classList.add('active_tag');
+};
+
+const changeImgPosition = () => {
+    const imgArr = document.querySelectorAll('.portfolio__img');
         imgArr[0].before(imgArr[10]);
         imgArr[1].before(imgArr[11]);
         imgArr[2].before(imgArr[3]);
         imgArr[3].before(imgArr[5]);
         imgArr[4].before(imgArr[6]);
-        document.querySelector('.button-container').classList.toggle('active');
-    })
-
-    document.querySelector('.image-container').addEventListener('click', (e) => {
-        document.querySelector('.image-container').classList.toggle('active-img');
-        document.querySelectorAll('.portfolio__img').forEach(el => 
-            el.classList.remove('active-img') 
-        );
-        e.target.classList.add('active-img');
-        document.querySelector('.image-container').classList.toggle('active-img');
-    })
-
-    SUBMIT.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.scrollTo(pageXOffset, 0);
-        const subject = document.getElementById('subject');
-        const describe = document.getElementById('textarea');
-
-        if (subject.value !== '') {
-            document.getElementById('subject-text').innerText = `Тема: ${subject.value.toString()}`;  
-        } else {
-            document.getElementById('subject-text').innerText = 'Без темы';
-        };
-
-        if (describe.value !== '') {
-            document.getElementById('describe').innerText = `Описание: ${describe.value.toString()}`; 
-        } else {
-            document.getElementById('describe').innerText = 'Без описания';
-        };
-
-        document.getElementById('message-block').classList.remove('hidden');
-    });
-        
-    CLOSE_BTN.addEventListener('click', () => {
-        document.getElementById('subject-text').innerText = '';
-        document.getElementById('describe').innerText = '';
-        document.getElementById('message-block').classList.add('hidden');
-    })
-
 };
 
-window.onload = () => {
-    load();
+const addClickBlackScreenHandler = () => {
+    document.querySelector('.home__btn_ver').addEventListener('click', () => {
+        document.querySelector('.black-screen__ver').classList.toggle('hidden');
+    })
+    
+    document.querySelector('.home__btn_gor').addEventListener('click', () => {
+        document.querySelector('.black-screen__gor').classList.toggle('hidden');
+    })
+
+    document.querySelector('.home__btn').addEventListener('click', () => {
+        document.querySelector('.black-screen').classList.toggle('hidden');
+    })
+    
+};
+
+const addClickSliderHandler = () => {
+    document.querySelector('.slider__btn_left').addEventListener('click', () => {
+        document.querySelector('.slide-first__container').classList.toggle('hidden');
+        document.querySelector('.slide-second__container').classList.toggle('hidden');
+      
+    })
+
+    document.querySelector('.slider__btn_right').addEventListener('click', () => {
+        document.querySelector('.slide-first__container').classList.toggle('hidden');
+        document.querySelector('.slide-second__container').classList.toggle('hidden');
+    })
+};
+
+const addClickImgHandler = () => {
+    document.querySelector('.image-container').addEventListener('click', (e) => {
+        if ( e.target.classList.contains('active_img')) {
+            e.target.classList.remove('active_img')
+        } else if (e.target.classList.contains('portfolio__img')) {
+            let clickedImg = e.target;
+            removeActiveImg();
+            selectClickedImg(clickedImg);
+        }
+    })
+};
+
+const removeActiveImg = () => {
+    let images = document.querySelectorAll('.portfolio__img');
+    images.forEach(img => img.classList.remove('active_img'));
+};
+
+const selectClickedImg = (img) => {
+    img.classList.add('active_img');
 }
+
+const addClickSubmitHandler = () => {
+    const submit = document.querySelector('.form__submit');
+    const name = document.querySelector('.form__name');
+    const email = document.querySelector('.form__email');
+    const subject = document.querySelector('.form__subject');
+    const describe = document.querySelector('.form__textarea');
+
+    submit.addEventListener('click', (e) => {
+        if (subject.value === 'Singolo' || subject.value === 'singolo') {
+            document.querySelector('.subject-text').innerText = `Тема: ${subject.value.toString()}`;  
+        } else {
+            document.querySelector('.subject-text').innerText = 'Без темы';
+        };
+
+        if (describe.value === 'Portfolio project') {
+            document.querySelector('.describe').innerText = `Описание: ${describe.value.toString()}`; 
+        } else {
+            document.querySelector('.describe').innerText = 'Без описания';
+        };
+      
+        if (name.value !== '' && email.validity.valid) {
+            e.preventDefault();
+            document.querySelector('.message-block').classList.remove('hidden');
+        }
+    });
+};
+
+const addClickModalCloseHandler = () => {
+    const close_btn = document.querySelector('.btn-ok');
+    close_btn.addEventListener('click', () => {
+        document.querySelector('.subject-text').innerText = '';
+        document.querySelector('.describe').innerText = '';
+        document.querySelector('.message-block').classList.add('hidden');
+    })
+};
+
 
 
